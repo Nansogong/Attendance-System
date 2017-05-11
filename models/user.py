c5b22fbe0ff59a6766b6823cfb4b4a49bd256baa
Jin-Soo Han <jinsu0411@gmail.com>
2017-05-11 13:55:26 +0900

    user(학생)와 lecture 사이를 연결해주는 테이블 작성

--- models/user.py
+++ models/user.py
@@ -51,8 +51,19 @@ class LectureDay(db.Model):
 
 class AttendanceManagement(db.Model):
     __tablename__ = 'attendance_management'
+
     id = db.Column(INTEGER(unsigned=True), primary_key=True)
     user_id = db.Column(INTEGER(unsigned=True))
     lecture_id = db.Column(INTEGER(unsigned=True))
     is_checked = db.Column(TINYINT(unsigned=True))
     created = db.Column(db.DateTime(), default=datetime.now(), index=True)
+
+
+class StudentLecture(db.Model):
+    __tablename__ = 'student_lecture'
+
+    student_id = db.Column(INTEGER(unsigned=True), db.ForeignKey("user.id"), primary_key=True)
+    lecture_id = db.Column(INTEGER(unsigned=True), db.ForeignKey("lecture.id"), primary_key=True)
+    accept_status = db.Column(TINYINT(unsigned=True))
+    updated = db.Column(db.DateTime())
+    created = db.Column(db.DateTime(), default=datetime.now())
