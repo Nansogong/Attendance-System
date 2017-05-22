8eb92053e7a6708e710cd5011d96ff38de3c671b
NamHyunSil <hsilnam3@gmail.com>
2017-05-22 15:53:34 +0900

    fixture을 통해 우선순위를 부여해 유저를 먼저 create하도록 하는 기능 추

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,3 +18,12 @@ def session():
     connection.close()
     session.remove()
 
+
+@pytest.fixture(scope='module')
+def create_user_first():
+    from models.user import User
+    password = 'alskdjfhghfj'
+    user = User(user_num=1234321, name='Pro fessor', email='professor@professor.com', password=password,
+                fingerprint='13531', type=User.PROFESSOR_TYPE)
+    user.create()
+    yield user
