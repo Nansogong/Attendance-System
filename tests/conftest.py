0bc8a47be3499ce9d45e2b668320f8d986752147
NamHyunSil <hsilnam3@gmail.com>
2017-06-02 16:10:54 +0900

    [추가] professor_list, accept_professr의 테스트를 위한 user를 create하는 fixture추가

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,11 +19,37 @@ def session():
     session.remove()
 
 
-@pytest.fixture(scope='module')
-def create_user_first():
+@pytest.fixture(scope='session')
+def user():
     from models.user import User
     password = 'alskdjfhghfj'
-    user = User(user_num=1234321, name='Pro fessor', email='professor@professor.com', password=password,
-                fingerprint='13531', type=User.PROFESSOR_TYPE)
-    user.create()
-    yield user
+    _user = User(user_num=1234321, name='Pro fessor', email='professor@professor.com', password=password,
+                 fingerprint='13531', type=User.PROFESSOR_TYPE)
+    _user.create()
+    yield _user
+
+
+@pytest.fixture(scope='session')
+def lecture(user):
+    from models.lecture import Lecture
+    _lecture = Lecture(professor_id=user.id, name="SWE", lecture_code="12364")
+    _lecture.create()
+    yield _lecture
+
+
+@pytest.fixture(scope='session')
+def professors():
+    from models.user import User
+    password = 'asdfasdfasdf'
+    _user = User(user_num=2345432, name='Pro fessor1', email='professor1@professor.com', password=password,
+                 fingerprint='35753', type=User.PROFESSOR_TYPE)
+    _user.create()
+    password = 'sdfgsdfgsdfg'
+    _user = User(user_num=3456543, name='Pro fessor2', email='professor2@professor.com', password=password,
+                 fingerprint='56765', type=User.ACCEPTED_PROFESSOR_TYPE)
+    _user.create()
+    password = 'dfghdfghdfgh'
+    _user = User(user_num=5678765, name='Pro fessor3', email='professor3@professor.com', password=password,
+                 fingerprint='78987', type=User.REJECTED_PROFESSOR_TYPE)
+    _user.create()
+    yield
