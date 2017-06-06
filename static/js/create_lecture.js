0d6f5bd895159387e02f4c21e612213bd8108e84
이재승 <jaeseung91@hanyang.ac.kr>
2017-06-06 16:18:17 +0900

    jQuery 테스팅위해서 살짝 수정

--- static/js/create_lecture.js
+++ static/js/create_lecture.js
@@ -1,14 +1,56 @@
 /**
  * Created by jaeseung on 2017. 6. 6..
  */
-var radio_lecture_amout = document.getElementsByName("radio_lecture_amount").value;
+// var radio_lecture_amout = document.getElementsByName("radio_lecture_amount").value;
+//
+// if (radio_lecture_amout == "1"){
+//     // 1번 시행
+// }
+// else if (radio_lecture_amout == "2"){
+//     // 2번 시행
+// }
+// else {
+//     // 3번 시행
+// }
 
-if (radio_lecture_amout == "1"){
-    // 1번 시행
-}
-else if (radio_lecture_amout == "2"){
-    // 2번 시행
-}
-else {
-    // 3번 시행
-}
+
+
+$(document).ready(function() {
+
+    // DOM 생성 완료 시 화면 숨김
+    // $("#changeM").hide();
+    $("#changeI").hide();
+    $("#changeH").hide();
+
+    // radio change 이벤트
+    $("input[name=radioName]").change(function() {
+        var radioValue = $(this).val();
+        if (radioValue == "M") {
+            $("#changeM").show();
+            $("#changeI").hide();
+            $("#changeH").hide();
+        } else if (radioValue == "I") {
+            $("#changeI").show();
+            $("#changeM").hide();
+            $("#changeH").hide();
+        } else if (radioValue == "H") {
+            $("#changeH").show();
+            $("#changeI").hide();
+            $("#changeM").hide();
+        }
+    });
+
+    // 서버에서 전달 받은 값으로 radio 버튼 변경
+    // $("#changeUpdateRadio").click(function() {
+    //     var resultValue = $("#radioId").val();
+    //     $("input[name=radioName][value=" + resultValue + "]").attr("checked", true);
+    // });
+
+    // 체크 되어 있는지 확인
+    var checkCnt = $("input[name=radioName]:checked").size();
+    if (checkCnt == 0) {
+        // default radio 체크 (첫 번째)
+        $("input[name=radioName]").eq(0).attr("checked", true);
+    }
+
+});
