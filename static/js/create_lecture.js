98d0f239d1203c91f948ae376635437f7038d588
이재승 <jaeseung91@hanyang.ac.kr>
2017-06-06 13:36:16 +0900

    create_lecture.js 시행될 함수 스켈레톤

---
+++ static/js/create_lecture.js
@@ -0,0 +1,14 @@
+/**
+ * Created by jaeseung on 2017. 6. 6..
+ */
+var radio_lecture_amout = document.getElementsByName("radio_lecture_amount").value;
+
+if (radio_lecture_amout == "1"){
+    // 1번 시행
+}
+else if (radio_lecture_amout == "2"){
+    // 2번 시행
+}
+else {
+    // 3번 시행
+}
